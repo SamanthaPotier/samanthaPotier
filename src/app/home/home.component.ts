ed1f909bad4513d3a22906628756e61c5fdd0d4c
Samantha Potier <samantha.potier@akka.digital>
2018-01-31 13:54:02 +0100

    structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
 import { Project }           from './../projects/project';
 import { ProjectService }    from './../services/project.service';
@@ -12,10 +13,21 @@ import { ProjectService }    from './../services/project.service';
 export class HomeComponent implements OnInit {
     projects: Project[] = [];
 
-    constructor(private projectService: ProjectService) { }
+    constructor(
+        private router: Router,
+        private projectService: ProjectService
+    ) { }
+
+    getProjects(): void {
+        this.projectService.getProjects().then(projects => this.projects = projects);
+    }
 
     ngOnInit(): void {
         this.projectService.getProjects()
             .then(projects => this.projects = projects.slice(1, 5));
+    }
+
+    onSelect(project: Project) {
+        this.router.navigate(['/projets', project.id]);
     }
 }
