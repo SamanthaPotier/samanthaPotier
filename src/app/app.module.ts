99a37826aee07ebf7ac400c71769031f979af14d
jeremyjsp <jeremy.jsp@akka.digital>
2018-02-01 16:30:38 +0100

    add helper ts for decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }                  from '@angular/core';
 import { BrowserModule }             from '@angular/platform-browser';
 import { BrowserAnimationsModule }   from '@angular/platform-browser/animations';
 import { HttpModule }                from '@angular/http';
-// import { BemModule }                 from 'angular-bem';
+import { BemModule }                 from 'angular-bem';
 
 import { ProjectService }     from './services/project.service';
 import { AppRoutingModule }   from './modules/app-routing.module';
@@ -30,7 +30,7 @@ import { PageNotFoundComponent }   from './pageNotFound.component';
         ProjectsModule,
         // ProjectsRoutingModule,
         AppRoutingModule,
-        // BemModule
+        BemModule
     ],
     declarations: [
         AppComponent,
